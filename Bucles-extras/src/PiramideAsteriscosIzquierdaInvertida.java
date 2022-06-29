a28249fb399594fb5faaff078c8ae9ecbe989409
Macarena <Macarena@DESKTOP-5A2HH1S>
2022-06-29 11:52:24 +0200

    con cambio nombres en clases

--- Bucles-extras/src/PiramideAsteriscosIzquierdaInvertida.java
+++ Bucles-extras/src/PiramideAsteriscosIzquierdaInvertida.java
@@ -12,7 +12,7 @@
 */
 import java.util.Scanner;
 
-public class PiramideAsteriscosDerecha {
+public class PiramideAsteriscosIzquierdaInvertida {
 
 	public static void main(String[] args) {
 		Scanner teclado=new Scanner(System.in);
